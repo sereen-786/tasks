3a45cee664e80e7eb507b99d945494d1b79c3c97
sereen <82365795+sereen-786@users.noreply.github.com>
2024-07-11 23:58:26 +0530

    Update task_4_solution.sql

--- Task_4/task_4_solution.sql
+++ Task_4/task_4_solution.sql
@@ -1,16 +1,26 @@
+-- Create a db if not exists
 create database if not exists weatherDB;
-use weatherDB;
-create table weather ( id INT PRIMARY KEY, recordData Date, temperature INT);
-insert into weather (id, recordData, temperature) values
-(1, '2015-01-01',10),
-(2, '2015-01-02',25),
-(3, '2015-01-03',20),
-(4, '2015-01-04',30);
 
+-- Use the db created
+use weatherDB;
+
+-- Create table to store weather data
+create table weather ( id INT PRIMARY KEY, recordData Date, temperature INT);
+
+-- Insert the given data into table
+insert into weather (id, recordData, temperature) values
+(1, '2015-01-01', 10),
+(2, '2015-01-02', 25),
+(3, '2015-01-03', 20),
+(4, '2015-01-04', 30);
+
+-- Check the table is populated with inserted data
 select * from weather;
 
+-- Self join based on id's where id is the unique key value in the table
 select w1.id 
 from weather w1 
 join weather w2 
 on w1.id = w2.id + 1 
 where w1.temperature > w2.temperature;
+
